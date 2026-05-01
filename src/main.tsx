841f13dbd55ed4754745a43e5da3e25c1e96e456
Qasim-Hassan <qasimhassan1947@gmail.com>
2026-05-01 21:44:45 +0500

    created separate dir for styling components

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
-import { ThemeContextProvider } from './ThemeContext.tsx';
+import { ThemeContextProvider } from './styling/ThemeContext.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
