22c4737bea8747e0d1295b2fc3114fcf6e9a5786
Qasim-Hassan <qasimhassan1947@gmail.com>
2026-05-01 22:25:15 +0500

    fixed theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./components/navbar";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<h1>Home</h1>} />
+        <Route path="/" element={<Home />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/components/navbar.tsx
@@ -0,0 +1,10 @@
+import ThemeToggle from "./toggleTheme";
+
+export default function Home() {
+  return (
+    <>
+      <ThemeToggle />
+      <h1>Landing Page</h1>
+    </>
+  );
+}
